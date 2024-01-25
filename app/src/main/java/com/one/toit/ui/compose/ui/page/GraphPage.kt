1379111348b99d2f9359e61db2bca8bb34eeaaaa
1-hee <onehee9710@gmail.com>
2024-01-25 21:59:48 +0900

    240125 상세 통계 페이지 막대 그래프 구성 및 추가

--- app/src/main/java/com/one/toit/ui/compose/ui/page/GraphPage.kt
+++ app/src/main/java/com/one/toit/ui/compose/ui/page/GraphPage.kt
@@ -1,26 +1,41 @@
 package com.one.toit.ui.compose.ui.page
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Switch
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.one.toit.R
+import com.one.toit.data.dto.ChartEntry
 import com.one.toit.ui.compose.style.black
+import com.one.toit.ui.compose.style.mono600
+import com.one.toit.ui.compose.style.navy400
+import com.one.toit.ui.compose.style.orange300
+import com.one.toit.ui.compose.style.purple300
+import com.one.toit.ui.compose.style.red300
 import com.one.toit.ui.compose.style.white
 import com.one.toit.ui.compose.ui.unit.BarGraphChart
-import com.one.toit.ui.compose.ui.unit.ChartBar
+import kotlin.random.Random
 
 // @Preview(showBackground = true)
 @Composable
@@ -32,24 +47,66 @@ fun GraphPage(
             .fillMaxSize()
             .background(white)
     ) {
-//        Text(
-//            text = stringResource(id = R.string.p_graph),
-//            style = MaterialTheme.typography.h1,
-//            textAlign = TextAlign.Center,
-//            color = black,
-//            modifier = Modifier.align(Alignment.Center)
-//        )
 
-        val brush = Brush.horizontalGradient(listOf(Color.Red, Color.Blue))
         Column(
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            //BarGraphChart()
-
-            BarGraphChart(
-
+            var graphMode by remember { mutableStateOf("막대 그래프") }
+            var isCheck by remember { mutableStateOf(false) }
+            // dummy
+            val test = listOf(
+                "9/1","9/2","9/3","9/4","9/5",
+                "9/6","9/7","9/8","9/9","9/10",
+                "9/10","9/11","9/12","9/13","9/14",
             )
+            val colorList = listOf(
+                black, red300, orange300, navy400, purple300
+            )
+            val testData = mutableMapOf<String, ChartEntry>()
+            test.forEach { date ->
+                /*
+                 val randomNumberInRange = Random.nextInt(1, 100)
+                 */
+                val randIdx = Random.nextInt(0, 4)
+                val volume = Random.nextInt(32,  128)
+                testData[date] = ChartEntry(volume, colorList[randIdx])
+            }
+            // dummy!
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ){
+                Text(
+                    text = graphMode,
+                    style = MaterialTheme.typography.caption.copy(
+                        color = mono600,
+                        fontSize = 8.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Switch(
+                    checked = isCheck,
+                    onCheckedChange = {
+                        isCheck = it
+                        graphMode = if(isCheck) "꺾은선 그래프" else "막대 그래프"
+                    }
+                )
+            }
+            if(!isCheck){
+                BarGraphChart(
+                    data = testData,
+                    durationMillis = 700,
+                    maxValue = 172,
+                )
+            }else {
+                // TODO 꺾은선 그래프!
+            }
+
         }
     }
 }
--- app/src/main/java/com/one/toit/ui/compose/ui/unit/BarGraphChart.kt
+++ app/src/main/java/com/one/toit/ui/compose/ui/unit/BarGraphChart.kt
@@ -8,6 +8,8 @@ import androidx.compose.animation.core.repeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,7 +22,11 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -34,83 +40,91 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.one.toit.data.dto.ChartEntry
 import com.one.toit.ui.compose.style.black
+import com.one.toit.ui.compose.style.mono600
 import com.one.toit.ui.compose.style.navy100
 import com.one.toit.ui.compose.style.navy300
+import com.one.toit.ui.compose.style.purple200
+import com.one.toit.ui.compose.style.purple300
+import com.one.toit.ui.compose.style.purple400
+import com.one.toit.ui.compose.style.purple500
 import com.one.toit.ui.compose.style.red300
+import com.one.toit.ui.compose.style.white
 import kotlinx.coroutines.delay
 import java.lang.Math.abs
 
-
-@Composable
-fun ChartBar(
-    modifier: Modifier = Modifier,
-    percentage: Int,
-    brush: Brush,
-    isHighlighted: Boolean = false
-) {
-    Box(
-        modifier = modifier
-            .clip(RoundedCornerShape(8.dp))
-            .height(kotlin.math.abs(percentage).dp)
-            .width(8.dp)
-            .background(brush)
-            .background(color = if (!isHighlighted) Color.Black.copy(alpha = 0.5f) else Color.Transparent)
-    )
-}
-
 @Composable
 fun BarGraphChart(
-//    radiusOuter: Dp = 140.dp,
-//    chartBarWidth: Dp = 35.dp,
     data:Map<String, ChartEntry> = mapOf(),
-    durationMillis:Int = 1000
+    durationMillis:Int = 1000,
+    maxValue: Int = 128,
 ) {
-
-    //Animation - it will be repeated 2 times
+    // 애니메이션
     val animatedProgress = remember { Animatable(0.001f) }
     LaunchedEffect(animatedProgress) {
         animatedProgress.animateTo(
             targetValue = 1f,
             animationSpec = tween(
-                durationMillis = 1000,
+                durationMillis = durationMillis,
                 delayMillis = 0,
                 easing = LinearOutSlowInEasing
             )
         )
     }
 
-
-
-    val brush = Brush.horizontalGradient(listOf(black, red300, navy300))
-
+    val scrollState = rememberScrollState()
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .wrapContentHeight(),
-        verticalAlignment = Alignment.CenterVertically,
+            .height(maxValue.dp)
+            .horizontalScroll(scrollState),
+        verticalAlignment = Alignment.Bottom,
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        repeat(3){
-            Canvas(modifier = Modifier
-                .wrapContentWidth()
-                .size(64.dp) ) {
+        data.forEach { (key, entry) ->
+            Column(
+                modifier = Modifier
+                    .wrapContentSize(),
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = entry.volume.toString(),
+                    style = MaterialTheme.typography.caption.copy(
+                        color = mono600,
+                        fontSize = 8.sp
+                    ),
+                    textAlign = TextAlign.Center,
+                )
 
-                val height = size.height
-                val startingPoint = Offset(size.width, height)
-                val endingPoint = Offset(size.width, height - (height*animatedProgress.value))
-
-                drawLine(
-                    brush = brush,
-                    strokeWidth = 8.dp.toPx(),
-                    start = startingPoint,
-                    end = endingPoint
+                Canvas(modifier = Modifier
+                    .wrapContentWidth()
+                    .height(entry.volume.dp)
+                ) {
+                    val height = size.height
+                    val startingPoint = Offset(size.width, height)
+                    val endingPoint = Offset(size.width, height - (height*animatedProgress.value))
+                    drawLine(
+                        purple300,
+                        strokeWidth = 16.dp.toPx(),
+                        start = startingPoint,
+                        end = endingPoint,
+                    )
+                }
+                Text(
+                    text = key,
+                    style = MaterialTheme.typography.caption.copy(
+                        color = mono600,
+                        fontSize = 12.sp
+                    ),
+                    textAlign = TextAlign.Center,
                 )
             }
         }
     }
-
-
 }
